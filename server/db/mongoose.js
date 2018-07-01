107a077dda010bb17ca7b6d5599e73588e96251a
Dhaneesh.GK <dhaneeshatservices@gmail.com>
2018-07-01 20:02:49 +0530

    mongoDb config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-url = 'mongodb://localhost:27017/ToDoApp'
+url = process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp'
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
